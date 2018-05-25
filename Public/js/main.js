40369582e551ac0fb51480e8c34638883528b661
AndyRoush <andyclan@gmail.com>
2018-05-24 18:32:47 -0700

    adjusted the CDN placement. Got pushpin working

---
+++ Public/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //Navbar hamburger init
+  $(".button-collapse").sideNav();
+  // parallax init
+  $('.parallax').parallax();
+
+  $('.pushpin').pushpin({
+    top: 750,
+    offset: 0
+  });
+
+
+});
